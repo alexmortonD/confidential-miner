285f427517b51ae151d703c36056097c2679d8ee
alexmortonD <KateyWienert120@hotmail.com>
2025-10-22 13:08:57 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -3,15 +3,24 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
+  const { deploy, execute } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const confidentialZama = await deploy("ConfidentialZama", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const minerNFT = await deploy("MinerNFT", {
+    from: deployer,
+    args: [confidentialZama.address],
+    log: true,
+  });
+
+  await execute("ConfidentialZama", { from: deployer, log: true }, "setMinerManager", minerNFT.address);
+
+  console.log(`ConfidentialZama: ${confidentialZama.address}`);
+  console.log(`MinerNFT: ${minerNFT.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_confidential_miner";
+func.tags = ["ConfidentialMiner"];
